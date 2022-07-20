5b8d2957e892ec9a80f8366b0cd6d639119351da
Stanislas Duprey <stanislasduprey@Stanislass-MBP.attlocal.net>
2022-07-20 14:04:08 -0700

    FRemoved launch.json

---
+++ part5/.layer0/lambda/layer0.config.js
@@ -0,0 +1,2 @@
+module.exports={connector:"@layer0/redwood"};
+//# sourceMappingURL=layer0.config.js.map
---
+++ part5/routes.js
@@ -0,0 +1,6 @@
+// This file was automatically added by layer0 init.
+// You should commit this file to source control.
+import { Router } from '@layer0/core'
+import { redwoodRoutes } from '@layer0/redwood'
+
+export default new Router().use(redwoodRoutes)
